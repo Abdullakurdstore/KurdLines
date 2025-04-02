41c9d578281dbd42a63003195133c90a7de1f812
Abdullakurdstore <abdullakurdstore@gmail.com>
2025-04-02 22:41:18 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+{
+  "name": "Kurd Line",
+  "subtitle": "A source for all of my apps",
+  "description": "Welcome to my source! Here you'll find all of my apps.",
+  "iconURL": "https://i.imgur.com/zlsuw2I.png",
+  "website": "https://kurdlines.com",
+  "patreonURL": "https://patreon.com/kurdlines.com",
+  "tintColor": "#6156e2",
+  "featuredApps": [
+    "ccomkurdlinesapp"
+  ],
+  "apps": [
+    {
+      "name": "Kurd Line",
+      "bundleIdentifier": "ccomkurdlinesapp",
+      "marketplaceID": "",
+      "developerName": "Abdulla Emad Nammet",
+      "subtitle": "An awesome app.",
+      "localizedDescription": "This is an awesome app only available on AltStore.",
+      "iconURL": "https://i.imgur.com/j9iIUZZ.png",
+      "tintColor": "#7f7f7f",
+      "category": "other",
+      "screenshots": [
+        "https://i.imgur.com/u1BEWbm.png"
+      ],
+      "versions": [],
+      "appPermissions": {
+        "entitlements": [],
+        "privacy": {
+          "NSAdvertisingAttributionReportEndpoint": "https://byteintl-ugpaidads.com",
+          "NSCameraUsageDescription": "You'll be able to record video.",
+          "NSUserTrackingUsageDescription": "This allows TikTok to provide you with a better ads experience.",
+          "NSPhotoLibraryAddUsageDescription": "To upload from or download to your device, allow access to your photos in your phone settings.",
+          "NSAppleMusicUsageDescription": "To select sound from your library, allow us to access your Apple Music.",
+          "NSSpeechRecognitionUsageDescription": "Speech Recognition will be used to help with your voice searches.",
+          "NSCalendarsUsageDescription": "Add events to your device calendar to get reminders when events start.",
+          "NSMicrophoneUsageDescription": "To record audio, allow us to access your Microphone.",
+          "NSLocationWhenInUseUsageDescription": "If location services are enabled, TikTok will collect, store and use your device's approximate location to improve your experience.",
+          "NSContactsUsageDescription": "Sync your contacts to easily find people you know on TikTok. Your contacts will only be used to help you connect with friends.",
+          "NSPhotoLibraryUsageDescription": "To upload from or download to your device, allow access to your photos in your phone settings."
+        }
+      },
+      "patreon": {}
+    }
+  ],
+  "news": []
+}
